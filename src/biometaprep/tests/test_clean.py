0e3302226930d63479d3fac6b4c31127afcef0bc
Liyana Saleem <Liyanaps17@gmail.com>
2026-02-12 22:06:08 +0000

    push CI workflow

--- src/biometaprep/tests/test_clean.py
+++
@@ -1,7 +0,0 @@
-from biometaprep.normalize.clean import _canonicalize_organism
-
-def test_canonicalize_human():
-    assert _canonicalize_organism("human") == "Homo sapiens"
-
-def test_canonicalize_mouse():
-    assert _canonicalize_organism("mouse") == "Mus musculus"
